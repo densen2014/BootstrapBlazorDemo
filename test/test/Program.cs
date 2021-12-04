769fdc9d29937f9cd0514802b6dd6b6d17988beb
Alex Chow <zhouchuanglin@gmail.com>
2021-12-04 14:08:36 +0100

    test 工程 net5 升级 net6格式

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -1,26 +1,52 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BootstrapBlazor.Components;
+using Microsoft.Extensions.Options;
+using test.Data;
 
-namespace test
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddBootstrapBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+//var cultureInfo = new CultureInfo("zh-CN");
+//CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+//CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+// 增加多语言支持配置信息. 知识点1
+builder.Services.AddRequestLocalization<IOptions<BootstrapBlazorOptions>>((localizerOption, blazorOption) =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    var supportedCultures = blazorOption.Value.GetSupportedCultures();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    localizerOption.SupportedCultures = supportedCultures;
+    localizerOption.SupportedUICultures = supportedCultures;
+});
+
+var app = builder.Build();
+
+// 启用本地化. 知识点2
+app.UseRequestLocalization(app.Services.GetService<IOptions<RequestLocalizationOptions>>()!.Value);
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapBlazorHub();
+    endpoints.MapFallbackToPage("/_Host");
+});
+
+app.Run();
