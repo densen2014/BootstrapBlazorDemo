073b9c61c1e3f16eaf2c46931246b2f8c329ddd6
Alex Chow <zhouchuanglin@gmail.com>
2021-12-17 13:57:25 +0100

    群友要的demo : Blazor.Magicodes.IE

---
+++ Blazor.Magicodes.IE/Data/WeatherForecast.cs
@@ -0,0 +1,21 @@
+﻿using Magicodes.ExporterAndImporter.Excel;
+using OfficeOpenXml.Table;
+using System.ComponentModel;
+
+namespace Blazor.Magicodes.IE.Data
+{
+    [ExcelImporter(IsLabelingError = true)]
+    [ExcelExporter(Name = "导入商品中间表", TableStyle = TableStyles.Light10, AutoFitAllColumn = true)]
+    public class WeatherForecast
+    {
+        [DisplayName("日期")]
+        public DateTime Date { get; set; }
+
+        [DisplayName("气温")]
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
---
+++ Blazor.Magicodes.IE/ImportExportsService.cs
@@ -0,0 +1,21 @@
+﻿using Magicodes.ExporterAndImporter.Core;
+using Magicodes.ExporterAndImporter.Excel;
+
+namespace Blazor.Magicodes.IE
+{
+    /// <summary>
+    /// 通用导入导出服务类
+    /// </summary>
+    public class ImportExportsService
+    {
+        public async Task<string> ExportToExcel<T>(string filePath, List<T> items = null) where T : class, new()
+        {
+            filePath = filePath ?? Path.Combine("D:\\", $"模板_{typeof(T).Name}.xlsx");
+
+            IExporter exporter = new ExcelExporter();
+            items = items ?? new List<T>();
+            var result = await exporter.Export(filePath, items);
+            return result.FileName;
+        }
+    }
+}
