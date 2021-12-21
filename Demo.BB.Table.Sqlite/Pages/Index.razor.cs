f9c22cc0b964f284602b838928956b387ac1ea0c
Alex Chow <zhouchuanglin@gmail.com>
2021-12-21 03:37:25 +0100

    添加: 表格ORM快速入门 工程

---
+++ Demo.BB.Table.Sqlite/Pages/Index.razor.cs
@@ -0,0 +1,39 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using AME.CommonUtils;
+using AME.Services;
+using AmeBlazor.Components;
+using Demo.BB.Table.Sqlite.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace Demo.BB.Table.Sqlite.Pages
+{
+    public partial class Index
+    {
+        // 由于使用了FreeSql ORM 数据服务,可以直接取对象 
+        [Inject] IFreeSql fsql { get; set; }
+
+        //用演示服务的数据初始化数据库
+        [Inject] WeatherForecastService ForecastService { get; set; }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                //懒的人,直接初始化一些数据用用
+                var res = fsql.Select<WeatherForecast>().Count();
+                if (res == 0)
+                {
+                    var forecasts = (await ForecastService.GetForecastAsync(DateTime.Now)).ToList();
+                    fsql.Insert<WeatherForecast>().AppendData(forecasts).ExecuteAffrows();
+                }
+            }
+        }
+
+    }
+}
+
+
---
+++ Demo.BB.Table.Sqlite/Program.cs
@@ -0,0 +1,53 @@
+﻿using Demo.BB.Table.Sqlite.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+// 增加 FreeSql ORM 数据服务操作类
+// 需要时打开下面代码
+// 需要引入 FreeSql 对 SQLite 的扩展包 FreeSql.Provider.Sqlite
+builder.Services.AddFreeSql(option =>
+{
+    option.UseConnectionString(FreeSql.DataType.Sqlite, builder.Configuration.GetConnectionString("bb"))
+#if DEBUG
+         //开发环境:自动同步实体
+         .UseAutoSyncStructure(true)
+         .UseNoneCommandParameter(true)
+         //调试sql语句输出
+         .UseMonitorCommand(cmd => System.Console.WriteLine(cmd.CommandText))
+#endif
+                    ;
+});
+
+var cultureInfo = new CultureInfo("zh-CN");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+builder.Services.AddDensenExtensions();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
