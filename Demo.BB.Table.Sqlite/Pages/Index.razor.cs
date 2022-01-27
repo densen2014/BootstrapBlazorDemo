cbd25925639ac293f864a7a2f29d6bdf5447bb5d
Alex Chow <zhouchuanglin@gmail.com>
2022-01-27 23:30:34 +0100

    代码生成table&DetailRow

--- Demo.BB.Table.Sqlite/Pages/Index.razor.cs
+++ Demo.BB.Table.Sqlite/Pages/Index.razor.cs
@@ -7,8 +7,11 @@
 using AME.CommonUtils;
 using AME.Services;
 using AmeBlazor.Components;
+using BootstrapBlazor.Components;
 using Demo.BB.Table.Sqlite.Data;
 using Microsoft.AspNetCore.Components;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Demo.BB.Table.Sqlite.Pages
 {
@@ -32,6 +35,131 @@ namespace Demo.BB.Table.Sqlite.Pages
                 }
             }
         }
+
+
+        /// <summary>
+        ///
+        /// </summary>
+        public class Foo
+        {
+            // 列头信息支持 Display DisplayName 两种标签
+
+            /// <summary>
+            ///
+            /// </summary>
+            [Key]
+            [Display(Name = "主键")]
+            [AutoGenerateColumn(Ignore = true)]
+            public int Id { get; set; }
+
+            /// <summary>
+            ///
+            /// </summary>
+            [Required(ErrorMessage = "{0}不能为空")]
+            [AutoGenerateColumn(Order = 10, Filterable = true, Searchable = true)]
+            [Display(Name = "姓名")]
+            public string? Name { get; set; }
+            /// <summary>
+            ///
+            /// </summary>
+            [Required(ErrorMessage = "请选择学历")]
+            [Display(Name = "学历")]
+            [AutoGenerateColumn(Order = 60)]
+            public EnumEducation? Education { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <returns></returns>
+            public static List<Foo> GenerateFoo(int count = 10) => Enumerable.Range(1, count).Select(i => new Foo()
+            {
+                Id = i,
+                Name = "Foo" + i.ToString(),
+                Education = random.Next(1, 100) > 50 ? EnumEducation.Primary : EnumEducation.Middel
+            }).ToList();
+            private static readonly Random random = new();
+
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public enum EnumEducation
+        {
+            /// <summary>
+            ///
+            /// </summary>
+            [Display(Name = "小学")]
+            Primary,
+
+            /// <summary>
+            ///
+            /// </summary>
+            [Display(Name = "中学")]
+            Middel
+        }
+        private class DetailRow
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            [DisplayName("主键")]
+            public int Id { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            [DisplayName("培训课程")]
+            [AutoGenerateColumn(Order = 10)]
+            public string Name { get; set; } = "";
+
+            /// <summary>
+            /// 
+            /// </summary>
+            [DisplayName("日期")]
+            [AutoGenerateColumn(Order = 20, Width = 180)]
+            public DateTime DateTime { get; set; }
+
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <returns></returns>
+            public static List<DetailRow> GenerateDetailRow(int count = 3) => Enumerable.Range(1, count).Select(i => new DetailRow()
+            {
+                Id = i,
+                Name = "DetailRow" + i.ToString(),
+                DateTime = DateTime.Now.AddDays(-count)
+            }).ToList();
+
+        }
+
+        private List<Foo>? Items { get; set; } = Foo.GenerateFoo();
+        private List<DetailRow>? DetailRows(int i) => DetailRow.GenerateDetailRow(i);
+
+        public RenderFragment TablesDetailRow() => builder =>
+        {
+            builder.OpenComponent(0, typeof(Table<Foo>));
+            builder.AddAttribute(1, nameof(Table<Foo>.Items), Items);
+            builder.AddAttribute(2, nameof(Table<Foo>.IsDetails), _ = true);
+            builder.AddAttribute(3, nameof(Table<Foo>.OnAfterRenderCallback), OnAfterRenderCallback());
+            builder.AddAttribute(4, nameof(Table<Foo>.DetailRowTemplate), GenerateDetailRow());
+            builder.AddAttribute(5, nameof(Table<Foo>.AutoGenerateColumns), true);
+            builder.CloseComponent();
+        }; 
+
+    Func<Table<Foo>, Task>? OnAfterRenderCallback()
+        {
+            return _ => Task.CompletedTask;
+        }
+
+
+        private RenderFragment<Foo> GenerateDetailRow()=>item => builder =>
+            {
+                builder.OpenComponent(0, typeof(Table<DetailRow>));
+                builder.AddAttribute(1, nameof(Table<DetailRow>.Items), DetailRows(item.Id));
+                builder.CloseComponent();
+            };
 
     }
 }
