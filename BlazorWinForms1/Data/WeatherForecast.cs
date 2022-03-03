b925394fc98ddcbb98329b79879478dce99c423c
Alex Chow <zhouchuanglin@gmail.com>
2022-03-04 00:21:11 +0100

    BlazorWinForms1 and BlazorWpf1

---
+++ BlazorWinForms1/Data/WeatherForecast.cs
@@ -0,0 +1,21 @@
+﻿using BootstrapBlazor.Components;
+using FreeSql.DataAnnotations;
+using System.ComponentModel;
+
+namespace Demo.BB.Table.Sqlite.Data
+{
+    [AutoGenerateClass(Searchable = true, Filterable = true, Sortable = true)]
+    public class WeatherForecast
+    {
+        [Column(IsIdentity = true)]
+        [DisplayName("序号")]
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
---
+++ BlazorWinForms1/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Demo.BB.Table.Sqlite.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BlazorWinForms1
+{
+    public static class Startup
+    {
+        public static IServiceProvider? Services { get; private set; }
+
+        public static void Init()
+        {
+            var host = Host.CreateDefaultBuilder()
+                           .ConfigureServices(WireupServices)
+                           .Build();
+            Services = host.Services;
+        }
+
+        private static void WireupServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.AddBlazorWebView();
+            services.AddSingleton<WeatherForecastService>();
+            // 增加 FreeSql ORM 数据服务操作类
+            // 需要时打开下面代码
+            // 需要引入 FreeSql 对 SQLite 的扩展包 FreeSql.Provider.Sqlite
+            services.AddFreeSql(option =>
+            {
+                option.UseConnectionString(FreeSql.DataType.Sqlite, "Data Source=test.db;")
+#if DEBUG
+                     //开发环境:自动同步实体
+                     .UseAutoSyncStructure(true)
+                     .UseNoneCommandParameter(true)
+                     //调试sql语句输出
+                     .UseMonitorCommand(cmd => System.Console.WriteLine(cmd.CommandText))
+#endif
+                    ;
+            });
+           services.AddDensenExtensions();
+
+        }
+    }
+}
